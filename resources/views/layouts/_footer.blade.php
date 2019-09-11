41575dfcdcdd83b16abad4c33d9ffb9e58356dfa
marsccyu <marsccyu@gmail.com>
2019-09-11 14:41:03 +0800

    第二章 1. 建立模板 Layout 2. 初始控制器頁面

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由 <a href="https://leo108.com" target="_blank">Mars</a>
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">Contact Us</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '首頁')
+
+@section('content')
+    <h1>Index</h1>
+@stop
