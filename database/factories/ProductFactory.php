a805eb129eebfb50a29f85af3bb916a6bb037143
marsccyu <marsccyu@gmail.com>
2019-09-12 17:09:56 +0800

    第五章 5.4 1. 用戶介面 - 商品列表

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,7 +6,23 @@ use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
+    $image = $faker->randomElement([
+        "images/1.jpg",
+        "images/2.jpg",
+        "images/3.jpg",
+        "images/4.jpg",
+        "images/5.jpg",
+        "images/6.jpg",
+    ]);
+
     return [
-        //
+        'title'        => $faker->word,
+        'description'  => $faker->sentence,
+        'image'        => $image,
+        'on_sale'      => true,
+        'rating'       => $faker->numberBetween(0, 5),
+        'sold_count'   => 0,
+        'review_count' => 0,
+        'price'        => 0,
     ];
 });
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,8 @@ Auth::routes(['verify' => true]);
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+
+    Route::redirect('/', '/products')->name('root');
+    Route::get('products', 'ProductsController@index')->name('products.index');
 });
+
