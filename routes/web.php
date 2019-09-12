82fe722bc2902ce01f4a3b73d826863691cd95ce
marsccyu <marsccyu@gmail.com>
2019-09-12 18:12:46 +0800

    第五章 5.9 1. 用戶介面 - 用戶收藏列表

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::redirect('/', '/products')->name('root');
     Route::get('products', 'ProductsController@index')->name('products.index');
     Route::get('products', 'ProductsController@index')->name('products.index');
-    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+    Route::get('products/{product}', 'ProductsController@show')->name('products.show')->where(['product' => '[0-9]+']);;
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
 
