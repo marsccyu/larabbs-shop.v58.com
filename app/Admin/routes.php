acb1490f071e1abf2958897b3af5ad998bcac0b0
marsccyu <marsccyu@gmail.com>
2019-09-12 16:15:54 +0800

    第五章 5.3 1. 管理後台 - 新增及修改商品功能

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -12,5 +12,12 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
 
+    /**
+     *  商品管理
+     */
     $router->get('products', 'ProductsController@index');
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
 });
