14127961a016f16cb3e81bd68dc057899bfcacf9
marsccyu <marsccyu@gmail.com>
2019-09-17 14:21:50 +0800

    第八章 8.1 1. 管理後台 - 訂單列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -20,4 +20,9 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+
+    /**
+     * 訂單管理
+     */
+    $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
 });
