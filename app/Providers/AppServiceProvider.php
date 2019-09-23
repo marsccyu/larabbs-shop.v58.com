b71c14144ad44c9926280b86b64c4be279950c8c
Mars-Home <bcawosxy@gmail.com>
2019-09-23 21:13:48 +0800

    L06 第三章 3.6 1. 用戶介面 - 頂部導航加入 view composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Carbon::setLocale('zh-TW');
+
+        // 當 Laravel 渲染 products.index 和 products.show 模板時，就會使用 CategoryTreeComposer 這個來注入類目樹變量
+        // 同時 Laravel 還支持通配符，例如 products.* 即代表當渲染 products 目錄下的模板時都執行這個 ViewComposer
+        \View::composer(['products.index', 'products.show'], \App\Http\ViewComposers\CategoryTreeComposer::class);
     }
 }
