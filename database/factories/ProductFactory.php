e6e98aec8da2bab66ff5e563eba42d3a04f5bec0
Mars-Home <bcawosxy@gmail.com>
2019-09-23 18:52:43 +0800

    L06 第三章 3.4 1. 後台管理 - 商品管理增加項目分類

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,6 +6,8 @@ use App\Models\Product;
 use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
+    $category = \App\Models\Category::query()->where('is_directory', false)->inRandomOrder()->first();
+
     $image = $faker->randomElement([
         "images/1.jpg",
         "images/2.jpg",
@@ -24,5 +26,8 @@ $factory->define(Product::class, function (Faker $faker) {
         'sold_count'   => 0,
         'review_count' => 0,
         'price'        => 0,
+        // 将取出的类目 ID 赋给 category_id 字段
+        // 如果数据库中没有类目则 $category 为 null，同样 category_id 也设成 null
+        'category_id'  => $category ? $category->id : null,
     ];
 });
