a738ebe971f93c15c3b33276bc4ede5bb4a4c226
Mars-Home <bcawosxy@gmail.com>
2019-09-26 14:59:25 +0800

    L06 第六章 6.2 1. 商品增加長標題

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'title', 'description', 'image', 'on_sale',
+        'title', 'long_title', 'description', 'image', 'on_sale',
         'rating', 'sold_count', 'review_count', 'price', 'type'
     ];
 
